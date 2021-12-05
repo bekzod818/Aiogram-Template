06c0117802fbea505b5645eb113d5996a60113f8
bekzod818 <bekzod030900@gmail.com>
2021-12-05 20:12:44 +0500

    guruh va shaxsiydagilarni filtrlash

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,9 +1,9 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
-
+from filters import IsPrivate
 from loader import dp
 
 
-@dp.message_handler(CommandStart())
+@dp.message_handler(IsPrivate(), CommandStart())
 async def bot_start(message: types.Message):
     await message.answer(f"Salom, {message.from_user.full_name}!")
