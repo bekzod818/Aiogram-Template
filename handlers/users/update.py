58a2e996ded184fb79452b0c4cb6d7030dc9be3c
bekzod818 <bekzod030900@gmail.com>
2021-12-17 14:55:49 +0500

    update

--- handlers/users/update.py
+++ handlers/users/update.py
@@ -1,7 +1,8 @@
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.builtin import Command
-from loader import dp, db
+from loader import dp, db, bot
+from data.config import ADMINS
 
 
 @dp.message_handler(Command("email"))
@@ -16,5 +17,6 @@ async def enter_email(message: types.Message, state: FSMContext):
     db.update_user_email(email=email, id=message.from_user.id)
     user = db.select_user(id=message.from_user.id)
     await message.answer("Muvaffaqiyatli qo'shildi.")
+    await bot.send_message(chat_id=ADMINS[0], text=user)
     # await message.answer(f"Baza yangilandi: {user}")
     await state.finish()
