919c56510aad674f3d85017ac5addb1c6d9c2f7f
bekzod818 <bekzod030900@gmail.com>
2021-12-22 00:57:53 +0500

    Add PostgreSQL

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,24 +1,27 @@
-import sqlite3
-
+import asyncpg
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
-from data.config import ADMINS
 from loader import dp, db, bot
+from data.config import ADMINS
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    name = message.from_user.full_name
-    # Foydalanuvchini bazaga qo'shamiz
+    name = message.from_user.username
     try:
-        db.add_user(id=message.from_user.id,
-                    name=name)
-        await message.answer(f"Xush kelibsiz! {name}")
-        # Adminga xabar beramiz
-        count = db.count_users()[0]
-        msg = f"{message.from_user.full_name} bazaga qo'shildi.\nBazada {count} ta foydalanuvchi bor."
-        await bot.send_message(chat_id=ADMINS[0], text=msg)
+        user = await db.add_user(
+            telegram_id=message.from_user.id,
+            full_name=message.from_user.full_name,
+            username=message.from_user.username,
+        )
 
-    except sqlite3.IntegrityError as err:
-        await bot.send_message(chat_id=ADMINS[0], text=f"{name} bazaga oldin qo'shilgan")
+        # ADMINGA xabar beramiz
+        count = await db.count_users()
+        msg = f"@{user[2]} bazaga qo'shildi.\nBazada {count} ta foydalanuvchi bor."
+        await bot.send_message(chat_id=ADMINS[0], text=msg)
+    except asyncpg.exceptions.UniqueViolationError:
+        # user = await db.select_user(telegram_id=message.from_user.id)
+        await bot.send_message(chat_id=ADMINS[0], text=f"@{name} bazaga oldin qo'shilgan")
+
+    await message.answer(f"Xush kelibsiz! @{name}")
