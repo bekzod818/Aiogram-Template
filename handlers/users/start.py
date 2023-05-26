c2dfca4b6b3bf3a2ffacc65c3b3c8f0e28b0e107
ChogirmaliYigit <chogirmali.yigit@gmail.com>
2023-05-26 15:51:50 +0500

    fixed: html to markdown

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -7,6 +7,20 @@ from utils.extra_datas import make_title
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
+    """
+            MARKDOWN V2                     |     HTML
+    link:   [Google](https://google.com/)   |     <a href='https://google.com/'>Google</a>
+    bold:   *Qalin text*                    |     <b>Qalin text</b>
+    italic: _Yotiq shriftdagi text_         |     <i>Yotiq shriftdagi text</i>
+
+
+
+                    **************     Note     **************
+    Markdownda _ * [ ] ( ) ~ ` > # + - = | { } . ! belgilari to'g'ridan to'g'ri ishlatilmaydi!!!
+    Bu belgilarni ishlatish uchun oldidan \ qo'yish esdan chiqmasin. Masalan  \.  ko'rinishi . belgisini ishlatish uchun yozilgan.
+    """
+
+
     full_name = message.from_user.full_name
     user = await db.select_user(telegram_id=message.from_user.id)
     if user is None:
